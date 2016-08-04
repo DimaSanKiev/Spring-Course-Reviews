6be8a6a8c02fab4c3b832c25e2ffc634e9581b38
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-08-05 01:17:53 +0300

    add relationships between Course and Review entities

--- src/main/java/edu/tutorials/course/Course.java
+++ src/main/java/edu/tutorials/course/Course.java
@@ -1,22 +1,39 @@
 package edu.tutorials.course;
 
 import edu.tutorials.core.BaseEntity;
+import edu.tutorials.review.Review;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-public class Course extends BaseEntity{
+public class Course extends BaseEntity {
     private String title;
     private String url;
+    @OneToMany(mappedBy = "course", cascade = CascadeType.ALL)
+    private List<Review> reviews;
 
     protected Course() {
         super();
+        reviews = new ArrayList<>();
     }
 
     public Course(String title, String url) {
         this();
         this.title = title;
         this.url = url;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void addReview(Review review) {
+        review.setCourse(this);
+        reviews.add(review);
     }
 
     public String getTitle() {
--- src/main/java/edu/tutorials/review/Review.java
+++ src/main/java/edu/tutorials/review/Review.java
@@ -1,17 +1,27 @@
 package edu.tutorials.review;
 
 import edu.tutorials.core.BaseEntity;
+import edu.tutorials.course.Course;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Review extends BaseEntity {
     private int rating;
     private String description;
+    @ManyToOne
+    private Course course;
 
     protected Review() {
         super();
     }
+
+    public Review(int rating, String description) {
+        this.rating = rating;
+        this.description = description;
+    }
+
     public int getRating() {
         return rating;
     }
@@ -26,5 +36,13 @@ public class Review extends BaseEntity {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
     }
 }
