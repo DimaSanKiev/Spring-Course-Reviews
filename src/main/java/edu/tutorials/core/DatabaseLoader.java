db770a07eae84331746db3494368603e5fc8acba
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-08-05 01:37:58 +0300

    add courses templates to DatabaseLoader

--- src/main/java/edu/tutorials/core/DatabaseLoader.java
+++ src/main/java/edu/tutorials/core/DatabaseLoader.java
@@ -2,10 +2,15 @@ package edu.tutorials.core;
 
 import edu.tutorials.course.Course;
 import edu.tutorials.course.CourseRepository;
+import edu.tutorials.review.Review;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
 
 @Component
 public class DatabaseLoader implements ApplicationRunner {
@@ -18,7 +23,39 @@ public class DatabaseLoader implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Course course = new Course("Building a RESTful Web Service", "https://spring.io/guides/gs/rest-service/");
+        Course course = new Course("Java Programming", "https://www.coursera.org/specializations/java-programming");
+        course.addReview(new Review(3, "Pretty good course for beginners."));
         courses.save(course);
+
+        String[] templates = {
+                "Up and running with %s",
+                "%s Basics",
+                "%s for Beginners",
+                "%s for Neckbeards",
+                "%s for Professionals",
+                "%s for Dummies",
+                "Under the hood: %s"
+        };
+
+        String[] buzzwords = {
+                "Spring REST Data",
+                "Java 9",
+                "Scala",
+                "Groovy",
+                "Hibernate",
+                "Spring"
+        };
+
+        List<Course> bunchOfCourses = new ArrayList<>();
+        IntStream.range(0, 100)
+                .forEach(i -> {
+                    String template = templates[i % templates.length];
+                    String buzzword = buzzwords[i % buzzwords.length];
+                    String title = String.format(template, buzzword);
+                    Course c = new Course(title, "http://www.example.com");
+                    c.addReview(new Review(i % 5, String.format("Moar %s please!", buzzword)));
+                    bunchOfCourses.add(c);
+                });
+        courses.save(bunchOfCourses);
     }
 }
